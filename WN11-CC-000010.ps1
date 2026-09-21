f8814d1473a1f52e1425ab8cb1eede341472d8b5
Eduardo Garcia <129396348+EdwardGarciaTV@users.noreply.github.com>
2026-09-20 21:26:04 -0500

    Clean up tested information and usage section
    Removed tested information and usage instructions from the script.

--- WN11-CC-000010.ps1
+++ WN11-CC-000010.ps1
@@ -14,14 +14,7 @@
     STIG-ID         : WN11-CC-000010
     Documentation   : https://stigaview.com/products/win11/v2r8/WN11-CC-000010/
 
-.TESTED ON
-    Date(s) Tested  : 
-    Tested By       : 
-    Systems Tested  : 
-    PowerShell Ver. : 
-
 .USAGE
-    Put any usage instructions here.
     Example syntax:
     PS C:\> (STIG-ID-WN11-CC-000010).ps1 
 
