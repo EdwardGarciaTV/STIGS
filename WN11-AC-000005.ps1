558922df1bcbf1fd3c110a8bec07fed8fa6fbf54
Eduardo Garcia <129396348+EdwardGarciaTV@users.noreply.github.com>
2026-09-20 21:24:13 -0500

    Clean up tested on section and usage instructions
    Removed outdated testing information and usage instructions.

--- WN11-AC-000005.ps1
+++ WN11-AC-000005.ps1
@@ -14,14 +14,7 @@
     STIG-ID         : WN11-AC-000005
     Documentation   : https://stigaview.com/products/win11/v2r8/WN11-AC-000005/
 
-.TESTED ON
-    Date(s) Tested  : 
-    Tested By       : 
-    Systems Tested  : 
-    PowerShell Ver. : 
-
 .USAGE
-    Put any usage instructions here.
     Example syntax:
     PS C:\> (STIG-ID-WN11-AC-000005).ps1 
 
